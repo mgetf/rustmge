18405f5fc84735e85ae228b1a224c918f65a88f5
tommy-mor <thmorriss@gmail.com>
2024-02-13 14:03:48 -0500

    keeps track of admin,

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix::{Actor, StreamHandler};
+use actix::{Actor, AsyncContext, StreamHandler};
 use actix_files::{Files, NamedFile};
 use actix_web::{get, post, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
 use actix_web_actors::ws;
@@ -32,14 +32,35 @@ struct AppState {
 
 struct Server {
     apiKey: String,
+    address: actix::Addr<ServerWs>,
 }
 
+use actix::prelude::*;
+
+// https://github.com/actix/examples/blob/master/websockets/chat/src/server.rs
 struct ServerWs {
     servers: Vec<Server>,
+    admin: Option<Server>,
 }
 impl Actor for ServerWs {
     type Context = ws::WebsocketContext<Self>;
 }
+
+#[derive(Message)]
+#[rtype(result = "()")]
+struct ForwardMessage {
+    message: String,
+}
+
+impl Handler<ForwardMessage> for ServerWs {
+    type Result = ();
+
+    fn handle(&mut self, msg: ForwardMessage, ctx: &mut Self::Context) {
+        println!("Forwarding message: {}", msg.message);
+        ctx.text(msg.message);
+    }
+}
+
 impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for ServerWs {
     fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
         match msg {
@@ -58,16 +79,30 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for ServerWs {
                         serverPort,
                         stvPort,
                     } => {
-                        self.servers.push(Server { apiKey: apiKey });
+                        if apiKey == "admin" {
+                            self.admin = Some(Server {
+                                apiKey: apiKey,
+                                address: ctx.address(),
+                            });
+                        } else {
+                            self.servers.push(Server {
+                                apiKey: apiKey,
+                                address: ctx.address(),
+                            });
+                        }
                     }
                     MessagePayload::AdminInstigateMatch {} => {
-                        let m: MessagePayload = MessagePayload::MatchDetails {
-                            arenaId: String::from("1"),
-                            p1Id: String::from("2"),
-                            p2Id: String::from("3"),
-                        };
-                        let p = serde_json::to_string(&m).unwrap();
-                        ctx.text(p);
+                        assert!(self.admin.as_mut().unwrap().address == ctx.address());
+
+                        for server in &self.servers {
+                            let m: MessagePayload = MessagePayload::MatchDetails {
+                                arenaId: String::from("1"),
+                                p1Id: String::from("2"),
+                                p2Id: String::from("3"),
+                            };
+                            let p = serde_json::to_string(&m).unwrap();
+                            server.address.do_send(ForwardMessage { message: p })
+                        }
                     }
                     MessagePayload::MatchDetails {
                         arenaId,
@@ -87,7 +122,14 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for ServerWs {
 }
 
 async fn server_route(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    let resp = ws::start(ServerWs { servers: vec![] }, &req, stream);
+    let resp = ws::start(
+        ServerWs {
+            servers: vec![],
+            admin: None,
+        },
+        &req,
+        stream,
+    );
     println!("server!!! {:?}", resp);
     resp
 }
