6b31b82f09f476c757c086a85e83ce31980217fe
tommy-mor <thmorriss@gmail.com>
2024-02-13 01:15:48 -0500

    parses the json

--- src/main.rs
+++ src/main.rs
@@ -2,50 +2,22 @@ use actix::{Actor, StreamHandler};
 use actix_files::{Files, NamedFile};
 use actix_web::{get, post, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
 use actix_web_actors::ws;
+use serde::{Deserialize, Serialize};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+#[derive(Debug, Deserialize)]
+#[serde(tag = "type", content = "payload")]
+enum MessagePayload {
+    ServerHello {
+        apiKey: String,
+        serverNum: String,
+        serverHost: String,
+        serverPort: String,
+        stvPort: String,
+    },
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
-// This struct represents state
 struct AppState {
     app_name: String,
-}
-
-struct AdminWs;
-impl Actor for AdminWs {
-    type Context = ws::WebsocketContext<Self>;
-}
-
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for AdminWs {
-    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
-        match msg {
-            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
-            Ok(ws::Message::Pong(_)) => println!("Pong received"),
-            Ok(ws::Message::Text(text)) => {
-                println!("Text received: {}", text);
-                ctx.text(text)
-            }
-            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
-            _ => (),
-        }
-    }
-}
-
-async fn admin_route(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    let resp = ws::start(AdminWs, &req, stream);
-    println!("{:?}", resp);
-    resp
 }
 
 struct ServerWs;
@@ -59,6 +31,10 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for ServerWs {
             Ok(ws::Message::Pong(_)) => println!("Pong received"),
             Ok(ws::Message::Text(text)) => {
                 println!("Text received: {}", text);
+                let parsed: MessagePayload =
+                    serde_json::from_str(&text).expect("unrecognized json");
+                println!("parsed: {:?}", parsed);
+
                 //ctx.text(text)
             }
             Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
@@ -84,12 +60,8 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(AppState {
                 app_name: String::from("Actix-web"),
             }))
-            .route("/ws", web::get().to(admin_route))
             .route("/tf2serverep", web::get().to(server_route))
             .route("/", web::get().to(index))
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
     })
     .bind(("0.0.0.0", 8080))?
     .run()
